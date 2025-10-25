af36ff706032360420eec1db6f436df5b34f5c14
shub-saini <anshsaini04@gmail.com>
2025-10-25 10:41:51 +0530

    clerk auth added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,24 +1,39 @@
 'use client';
 
 import { Button } from '@workspace/ui/components/button';
-import { useQuery, useMutation } from 'convex/react';
+import {
+  useQuery,
+  useMutation,
+  Authenticated,
+  Unauthenticated,
+} from 'convex/react';
 import { api } from '@workspace/backend/_generated/api';
+import { SignInButton, UserButton } from '@clerk/nextjs';
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-svh'>
-      <Button
-        onClick={() => {
-          addUser();
-        }}
-      >
-        Add User
-      </Button>
-      <p>apps/web</p>
-      <p>{JSON.stringify(users, null, 2)}</p>
-    </div>
+    <>
+      <Authenticated>
+        <div className='flex flex-col items-center justify-center min-h-svh'>
+          <Button
+            onClick={() => {
+              addUser();
+            }}
+          >
+            Add User
+          </Button>
+          <p>apps/web</p>
+          <UserButton />
+          <p>{JSON.stringify(users, null, 2)}</p>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        Must be signed in
+        <SignInButton />
+      </Unauthenticated>
+    </>
   );
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,22 @@
 'use client';
 
 import { Button } from '@workspace/ui/components/button';
-import { useQuery } from 'convex/react';
+import { useMutation, useQuery } from 'convex/react';
 import { api } from '@workspace/backend/_generated/api';
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
   return (
     <div className='flex flex-col items-center justify-center min-h-svh'>
+      <Button
+        onClick={() => {
+          addUser();
+        }}
+      >
+        Add User
+      </Button>
       <p>apps/widget</p>
       <p>{JSON.stringify(users, null, 2)}</p>
     </div>

64e1ad58605f53d482e3cd48d504d64005179343
shub-saini <anshsaini04@gmail.com>
2025-11-05 09:52:56 +0530

    widget auth added

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Widgetfooter } from '../components/widget-footer';
-import { WidgetHeader } from '../components/widget-header';
+// import { Widgetfooter } from '../components/widget-footer';
+import { WidgetAuthScreen } from '../screens/widget-auth-screen';
 
 interface Props {
   organizationId: string;
@@ -10,14 +10,8 @@ interface Props {
 export const WidgetView = ({ organizationId }: Props) => {
   return (
     <main className='min-h-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted'>
-      <WidgetHeader>
-        <div className='flex flex-col justify-between gap-y-2 px-2 py-6'>
-          <p className='text-3xl'>Hi there 👋</p>
-          <p className='text-lg'>How can we help you today?</p>
-        </div>
-      </WidgetHeader>
-      <div className='flex flex-1'>Widget View: {organizationId}</div>
-      <Widgetfooter />
+      <WidgetAuthScreen />
+      {/* <Widgetfooter /> */}
     </main>
   );
 };

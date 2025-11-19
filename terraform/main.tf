3cab8993e3c3a5db1e9d20bc1e420eb9c6023d58
shub-saini <anshsaini04@gmail.com>
2025-11-19 09:54:51 +0530

    ci-cd.yml updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,22 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Web"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Widget"
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
